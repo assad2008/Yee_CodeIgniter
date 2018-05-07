f8b4b36fcba757f5bf36ff0e6c09b5f8ae4ba805
assad <rlk002@gmail.com>
2018-05-07 21:33:44 +0800

    更新包2018-5-7 21:33:58

---
+++ data/views/cb/cb07cc06dea673b316eae7b93f71f5505e4bb398f4c784c78abc78ca663d1fc0.php
@@ -0,0 +1,64 @@
+<?php
+
+/* main.html */
+class __TwigTemplate_6786f2cb33ab2c23eec5e94e70231e5e6fd4275d2c01cbff2c2f429c800b96b4 extends Twig_Template
+{
+    private $source;
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+    <head>
+        <meta charset=\"utf-8\">
+    </head>
+    <body>
+    ";
+        // line 7
+        echo twig_escape_filter($this->env, ($context["welcome"] ?? null), "html", null, true);
+        echo "
+    </body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "main.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 7,  23 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<!DOCTYPE html>
+<html lang=\"en\">
+    <head>
+        <meta charset=\"utf-8\">
+    </head>
+    <body>
+    {{ welcome }}
+    </body>
+</html>", "main.html", "/www/wwwroot/ci.yeedev.xyz/application/views/main.html");
+    }
+}
