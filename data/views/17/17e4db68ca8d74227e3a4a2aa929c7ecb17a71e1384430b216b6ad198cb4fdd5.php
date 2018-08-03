5000d1da5d9b43278a443a0e82583a08f0e71711
assad <rlk002@gmail.com>
2018-08-03 18:21:07 +0800

    delete session files and log files

---
+++ data/views/17/17e4db68ca8d74227e3a4a2aa929c7ecb17a71e1384430b216b6ad198cb4fdd5.php
@@ -0,0 +1,64 @@
+<?php
+
+/* main.html */
+class __TwigTemplate_22f312904059fcb331262a7da20b8a423700e7675e1b6cee214c7119f2aa55b1 extends Twig_Template
+{
+    private $source;
+
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+    <head>
+        <meta charset=\"utf-8\">
+    </head>
+    <body>
+    ";
+        // line 7
+        echo twig_escape_filter($this->env, ($context["welcome"] ?? null), "html", null, true);
+        echo "
+    </body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "main.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  31 => 7,  23 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Twig_Source("<!DOCTYPE html>
+<html lang=\"en\">
+    <head>
+        <meta charset=\"utf-8\">
+    </head>
+    <body>
+    {{ welcome }}
+    </body>
+</html>", "main.html", "/www/wwwroot/ci.yeedev.xyz/application/views/main.html");
+    }
+}
