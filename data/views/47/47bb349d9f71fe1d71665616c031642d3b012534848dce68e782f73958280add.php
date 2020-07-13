5733aacf435a189cfa90e5b048c806545d5eed14
assad2008 <rlk002@gmail.com>
2020-07-13 20:46:10 +0800

    更新包2020-7-13 20:46:11

---
+++ data/views/47/47bb349d9f71fe1d71665616c031642d3b012534848dce68e782f73958280add.php
@@ -0,0 +1,80 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* main.html */
+class __TwigTemplate_a5d785bab8f6ee264eb9b7d506190d869773777ce7f0b0a76f5bfba0c59fb101 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo "<!DOCTYPE html>
+<html lang=\"en\">
+    <head>
+        <meta charset=\"utf-8\">
+    </head>
+    <body>
+    ";
+        // line 7
+        echo twig_escape_filter($this->env, ($context["welcome"] ?? null), "html", null, true);
+        echo "，";
+        echo twig_escape_filter($this->env, twig_get_attribute($this->env, $this->source, ($context["post"] ?? null), "post_title", [], "any", false, false, false, 7), "html", null, true);
+        echo "
+    </body>
+</html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "main.html";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  45 => 7,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("<!DOCTYPE html>
+<html lang=\"en\">
+    <head>
+        <meta charset=\"utf-8\">
+    </head>
+    <body>
+    {{ welcome }}，{{ post.post_title }}
+    </body>
+</html>", "main.html", "/www/wwwroot/ci.yeedev.xyz/application/views/main.html");
+    }
+}
